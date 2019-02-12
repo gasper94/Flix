f49502fb60ac441a11acbdcf6be9d9d08ef56209
gasper94 <ulimarmol94@gmail.com>
2019-02-12 00:54:00 -0800

    last submition

---
+++ MovieApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  MovieApp
+//
+//  Created by Ulises Martinez on 2/11/19.
+//  Copyright © 2019 Ulises Martinez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
